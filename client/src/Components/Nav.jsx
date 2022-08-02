12c1c8cec517750db929bc93e5035da08c442705
crisvalhas <cristianrubiles@gmail.com>
2022-08-02 18:38:16 -0300

    make it better

--- client/src/Components/Nav.jsx
+++ client/src/Components/Nav.jsx
@@ -14,6 +14,7 @@ export default function Nav() {
         dispatch(getAllVideogames());
     }
     return (
+        
         <header className="navbar">
             <div>
                 <Link to="/" >
